2c258979b682f3b8943060ce82d38ffb21a523d8
nCoLlInS24 <98181030+nCoLlInS24@users.noreply.github.com>
2023-10-26 00:46:46 +0000

    imported flask login_user

--- App/views/staff.py
+++ App/views/staff.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
 from flask_jwt_extended import jwt_required, current_user as jwt_current_user
-from flask_login import current_user, login_required
+from flask_login import current_user, login_required, login_user
 from flask import request, jsonify
 
 
