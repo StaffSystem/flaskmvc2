830ebb841a7ac46efa4647db677b2e7513695821
nCoLlInS24 <natankcollins@gmail.com>
2023-10-27 21:13:40 +0000

    reviewList_views was missing a s, i removed the s

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -4,7 +4,7 @@ from .user import user_views
 from .staff import staff_view
 from .index import index_views
 from .auth import auth_views
-from .reviewList import reviewList_views
+from .reviewList import reviewList_view
 
-views = [user_views, index_views, auth_views,staff_view, reviewList_views] #something is staff view not working
+views = [user_views, index_views, auth_views,staff_view, reviewList_view] #something is staff view not working
 # blueprints must be added to this list
